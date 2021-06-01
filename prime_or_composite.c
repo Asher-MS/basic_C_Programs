508ab56f53971271ac76af722cfc4ebe03e3fa4c
Asher Mathews <msasher123@gmail.com>
2021-06-01 09:37:42 +0530

    added n_tree.c

--- prime_or_composite.c
+++ prime_or_composite.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-
+#include<math.h>
 
 int main(){
 	int n;
 	printf("Enter the Number\n");
 	scanf("%d",&n);
-	for(int i=2;i<=(n/2);++i){
+	for(int i=2;i<=pow(n,0.5);++i){
 		if(n%i==0){
 			printf("It is Composite\n");
 			return 0;
