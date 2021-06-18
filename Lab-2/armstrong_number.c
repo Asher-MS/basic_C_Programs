899d848e090f599467bfac666b33756e2ebddae5
Sanjay S J <78257068+sanjaysj6282@users.noreply.github.com>
2021-06-18 09:17:19 +0530

    Update armstrong_number.c

--- Lab-2/armstrong_number.c
+++ Lab-2/armstrong_number.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     int no = 153;
-    
+
     int k=no;
-    int t=0;
-    int temp;
+    int cnt=0;
     while(no>0){
-        temp=no%10;
-        t += temp*temp*temp;
+        ++cnt;
 
         no=no/10;
     }
 
+    // printf("%i", cnt);
+    
+    int s=k;
+    int t=0;
+    int temp;
+    while(k>0){
+        temp=k%10;
+        // printf("%i \n", temp);
+        t += pow(temp, cnt);
+        // printf("%i \n", t);
 
+        k=k/10;
+    }
 
-    if (k == t)
+    // printf("%i \n", s);
+    // printf("%i \n", t);
+
+    if (s == t)
     {
         printf("The Number is Armstrong");
     }
