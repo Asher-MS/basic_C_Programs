d8b7b9355a7b6f62a818e0eace8f182a1481a3a9
Asher Mathews <msasher123@gmail.com>
2021-09-02 10:02:49 +0530

    Added Some 2D array programs

--- product_of_matrices.c
+++ product_of_matrices.c
@@ -10,6 +10,8 @@ int main()
 	scanf("%d %d",&x1,&y1);
 	printf("Enter the dimension of second matrix");
 	scanf("%d %d",&x2,&y2);
+	if(y1==x2)
+	{
 	printf("Enter the first matrix");
 	for(int i=0;i<x1;++i)
 	{
@@ -18,6 +20,7 @@ int main()
 			scanf("%d",&arr1[i][j]);
 		}
 	}
+	printf("Enter the second matrix");
 	for(int i=0;i<x2;++i)
         {
                 for (int j=0;j<y2;++j)
@@ -26,21 +29,28 @@ int main()
                 }
         }
 	
-	for(int i=0;i<x1;++i)
-	{
-		
-		for(int j=0;j<y1;++j)
-		{	
-			int temp_sum=0;
-			for(int k=0;k<x2;++k)
-			{
-				temp_sum+=arr1[i][k]*arr2[k][j];
+
+
+		for(int i=0;i<x1;++i)
+		{
+			
+			for(int j=0;j<y1;++j)
+			{	
+				int temp_sum=0;
+				for(int k=0;k<x2;++k)
+				{
+					temp_sum+=arr1[i][k]*arr2[k][j];
+
+				}
+				result[i][j]=temp_sum;
 
 			}
-			result[i][j]=temp_sum;
-
+			
 		}
-		
+	}
+	else{
+		printf("Multiplication not possible");
+		return 0;
 	}
 
 	for(int i=0;i<x1;++i)
