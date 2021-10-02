0c6d3f62da3f5bc0a1900399766b6b516f7e3fb0
Asher Mathews <msasher123@gmail.com>
2021-10-02 12:30:24 +0530

    made some changes to lab-8 programs

--- Lab-8/alphabet_printer.c
+++ Lab-8/alphabet_printer.c
@@ -1,21 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
 	char alphabets[26];
-	for(int i=0;i<26;++i)
+	for (int i = 0; i < 26; ++i)
 	{
-		alphabets[i]=(char)(65+i);
+		alphabets[i] = (char)(65 + i);
 	}
-	int i=0;
-	for(char* c=alphabets;i<26;++i)
+	int i = 0;
+	for (char *c = alphabets; i < 26; ++i)
 	{
-		printf("%c\n",*(c+i));
+		printf("%c Ascii value:%i \n", *(c + i), *(c + i));
 	}
 
 	return 0;
-
 }
--- Lab-8/maximum_element.c
+++ Lab-8/maximum_element.c
@@ -1,28 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int* max_element(int* arr,int n)
+int *max_element(int *arr, int n)
 {
-	int* max;
-	max=arr;
-	for(int i=0;i<n;++i)
+	int *max;
+	max = arr;
+	for (int i = 0; i < n; ++i)
 	{
-		if(*(arr+i)>*max)max=(arr+i);
+		if (*(arr + i) > *max)
+			max = (arr + i);
 	}
 	return max;
 }
 
 int main()
-{	
+{
 	int arr[100];
 	int n;
 	printf("Enter the size of the array");
-	scanf("%i",&n);
+	scanf("%i", &n);
 	printf("Enter the array");
-	for(int i=0;i<n;++i)scanf("%i",&arr[i]);
-	int* max=max_element(arr,n);
-	printf("largest Element is %i and Address is %i",*max,max);
-	
+	for (int i = 0; i < n; ++i)
+		scanf("%i", &arr[i]);
+	int *max = max_element(arr, n);
+	printf("largest Element is %i and Address is %p", *max, max);
 
 	return 0;
 }
