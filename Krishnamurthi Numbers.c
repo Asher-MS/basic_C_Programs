be7e9d22026e4c26b7bda738e514cb719e0840ca
Asher Mathews <msasher123@gmail.com>
2021-10-20 16:09:16 +0530

    Update Krishnamurthi Numbers.c
    fixed the code formatting

--- Krishnamurthi Numbers.c
+++ Krishnamurthi Numbers.c
@@ -1,32 +1,28 @@
-/*C Program to print all Krishnamurti numbers in a given range.*/
 #include<stdio.h>
-int main(){
-int a,b,num,n,d,i,f,sum;
-printf("Enter the lower limit: ");
-scanf("%i",&a);
-printf("Enter the upper limit: ");
-scanf("%i",&b);
-printf("The Krishnamurthy numbers in the range are :\n");
-for(n=a;n<=b;n++)
-{
-if(n==0)
-continue;
-sum=0;
-num=n;
-while(num!=0)
-{
-d=num%10;
-f=1;
-for(i=2;i<=d;i++)
-{
-f *= i;
-}
-sum += f;
-num /=10;
-}
-if(n==sum)
-{
-printf("%i\n",n);
-}
-}
+
+int main() {
+  int a, b, num, n, d, i, f, sum;
+  printf("Enter the lower limit: ");
+  scanf("%i", & a);
+  printf("Enter the upper limit: ");
+  scanf("%i", & b);
+  printf("The Krishnamurthy numbers in the range are :\n");
+  for (n = a; n <= b; n++) {
+    if (n == 0)
+      continue;
+    sum = 0;
+    num = n;
+    while (num != 0) {
+      d = num % 10;
+      f = 1;
+      for (i = 2; i <= d; i++) {
+        f *= i;
+      }
+      sum += f;
+      num /= 10;
+    }
+    if (n == sum) {
+      printf("%i\n", n);
+    }
+  }
 }
