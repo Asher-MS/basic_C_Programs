ccc4af3334a5636df9427002bc6c9780808a5208
AnshDeep Yadav <59330323+AnshDeep24@users.noreply.github.com>
2021-10-24 13:34:43 +0530

    added uppercase and lowercase funtion in c

---
+++ Intermediate/toupper.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char c;
+    c=a;
+    printf("%c after conversions  %c", c, toupper(c));
+    c=B;
+    printf("%c after conversions  %c", c, toupper(c));
+}
